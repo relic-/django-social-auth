48a3d258144690f81e450141260985ff505d9b8d
Igor <relic.lv@gmail.com>
2012-02-06 03:50:07 +0000

    Added basic Steam Community OpenID backend

---
+++ social_auth/backends/contrib/steam.py
@@ -0,0 +1,32 @@
+"""
+SteamCommunity OpenID support
+
+No extra configurations are needed to make this work.
+"""
+import logging
+logger = logging.getLogger(__name__)
+
+from social_auth.backends import OpenIDBackend, OpenIdAuth
+
+
+STEAM_OPENID_URL = 'http://steamcommunity.com/openid'
+
+
+class SteamBackend(OpenIDBackend):
+    """Steam OpenID authentication backend"""
+    name = 'steam'
+
+
+class SteamAuth(OpenIdAuth):
+    """Steam OpenID authentication"""
+    AUTH_BACKEND = SteamBackend
+
+    def openid_url(self):
+        """Return Yahoo OpenID service url"""
+        return STEAM_OPENID_URL
+
+
+# Backend definition
+BACKENDS = {
+    'steam': SteamAuth,
+}
